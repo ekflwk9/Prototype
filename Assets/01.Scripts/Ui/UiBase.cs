4f0d7b051d23dba4b91164637faa59424d5b6b4b
ekflwk9 <ekflwk9@naver.com>
2025-08-20 18:30:38 +0900

    [feat] UiManager스크립트 생성

---
+++ Assets/01.Scripts/Ui/UiBase.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public abstract class UiBase : MonoBehaviour
+{
+    public virtual void On() => this.gameObject.SetActive(true);
+    public virtual void Off() => this.gameObject.SetActive(false);
+}
---
+++ Assets/01.Scripts/Ui/UiManger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiManger : MonoBehaviour
+{
+    public static UiManger instance;
+    private Dictionary<Type, UiBase> ui = new();
+
+    //public T Get<T>() where T : UiBase
+    //{
+    //    var key = typeof(T);
+    //    if(ui.ContainsKey(key)) return ui as T;
+    //}
+}
